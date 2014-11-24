bbd65e45f22cc91bc82efa9ff8df41756e6585b0
Caio Ribeiro Pereira <caio.pereira@concretesolutions.com.br>
2014-11-24 18:02:33 +0000

    Outputing a readme file

--- src/readme.go
+++ src/readme.go
@@ -3,8 +3,21 @@ package main
 import (
   "fmt"
   "os"
+  "bufio"
+  "log"
   "github.com/codegangsta/cli"
 )
+
+func isConfirmed(value string) bool {
+  return value == "y" || value == "Y" || value == "yes"
+}
+
+func check(err error) {
+  if err != nil {
+    log.Fatal(err)
+    panic(err)
+  }
+}
 
 func main() {
   app := cli.NewApp()
@@ -16,15 +29,40 @@ func main() {
       ShortName: "i",
       Usage: "Generates README.md based on some questions you will answer",
       Action: func(c *cli.Context) {
+        in := bufio.NewReader(os.Stdin)
         fmt.Println("Project's name:")
-        fmt.Println("Project's description:")
-        fmt.Println("Project's site:")
+        prjName, _ := in.ReadString('\n')
+        fmt.Println("Project's short description:")
+        prjDesc, _ := in.ReadString('\n')
         fmt.Println("Author's name:")
+        authorName, _ := in.ReadString('\n')
         fmt.Println("Author's e-mail:")
-        fmt.Println("This project has instalation's instruction? (yes)")
+        authorEmail, _ := in.ReadString('\n')
+        fmt.Println("This project has setup's instruction? (yes)")
+        prjSetup, _ := in.ReadString('\n')
         fmt.Println("This project has development's instruction? (yes)")
+        prjDev, _ := in.ReadString('\n')
         fmt.Println("This project has test's instruction? (yes)")
+        prjTest, _ := in.ReadString('\n')
         fmt.Println("License's name:")
+        prjLicense, _ := in.ReadString('\n')
+        file, err := os.Create("README.md")
+        check(err)
+        out := bufio.NewWriter(file)
+        out.WriteString("# "+ prjName +"\n\n")
+        out.WriteString(prjDesc +"\n\n")
+        out.WriteString("## About\n\nWrite here about what is your project...\n\n")
+        if isConfirmed(prjSetup) {
+          out.WriteString("## Instalation\n\nWrite here about how install your project  \n`write here some code if is necessary!`\n\n")
+        }
+        if isConfirmed(prjDev) {
+          out.WriteString("## Development\n\nWrite here about how setup your project environment for development purposes  \n`write here some code if is necessary!`\n\n")
+        }
+        if isConfirmed(prjTest) {
+          out.WriteString("## Running test\n\nWrite here about how execute test tasks into your project  \n`write here some code if is necessary!`\n\n")
+        }
+        out.WriteString("## Author\n\n"+ authorName +" <"+ authorEmail +">  \nLicensed by "+ prjLicense)
+        out.Flush()
       },
     },
   }
